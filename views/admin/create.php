a77cc01c4053dad20cac1cd150ab554b92d04376
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2025-03-02 23:01:35 -0500

    Enh: Use create modal
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/admin/create.php
+++ views/admin/create.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use humhub\widgets\Button;
+use humhub\widgets\ModalDialog;
 
 /**
  * @var $this yii\web\View
@@ -11,14 +11,11 @@ use humhub\widgets\Button;
 $this->title = Yii::t('FirewallModule.base', 'Create Firewall Rule');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('FirewallModule.base', 'Firewall Rules'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <?= Yii::t('FirewallModule.base', '<strong>Create</strong> firewall rule'); ?>
+
+<?php ModalDialog::begin(['header' => $this->title]); ?>
+    <div class="modal-body">
+        <?= $this->render('_form', ['model' => $model]) ?>
     </div>
-    <div class="panel-body">
-        <?= $this->render('_form', [
-            'model' => $model,
-        ]); ?>
-    </div>
-</div>
+<?php ModalDialog::end(); ?>
